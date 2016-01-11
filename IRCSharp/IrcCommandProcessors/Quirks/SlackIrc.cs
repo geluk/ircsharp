ddaaef2e270112ad069a379aa37dc0c93a1cdbcd
Johan Geluk <jhgeluk@gmail.com>
2016-01-11 12:19:04 +0100

    Add documentation to SlackIrcProcessor

--- IRCSharp/IrcCommandProcessors/Quirks/SlackIrc.cs
+++ IRCSharp/IrcCommandProcessors/Quirks/SlackIrc.cs
@@ -12,6 +12,10 @@ namespace IRCSharp.IrcCommandProcessors.Quirks
 {
 	public class SlackIrcProcessor : DataProcessor
 	{
+		/// <summary>
+		/// Keeps replacing <paramref name="pattern"/> with <paramref name="replacement"/> in <paramref name="input"/>
+		/// until <paramref name="input"/> no longer matches <paramref name="pattern"/>, then returns the result.
+		/// </summary>
 		private string RecursiveReplace(string input, string pattern, string replacement)
 		{
 			string currentMessage;
@@ -26,8 +30,20 @@ namespace IRCSharp.IrcCommandProcessors.Quirks
 
 		public override IrcMessage ProcessMessage(IrcMessage message)
 		{
+			// Whenever it encounters a URL, the Slack IRC relay does two things:
+			// First, it inserts an HTTP URL after everything it thinks might be formatted like a domain name (or an incorrectly formatted URL),
+			// With the inserted URL pointing to the previously mentioned domain.
+			// Additionally, it adds some spaces around the URL, just for good measure.
+			// Second, while it doesn't add a URL after any correctly formatted URLs, it will, in its infinite wisdom,
+			// nonetheless decide to add a space after that URL.
+			// Guess who's going to have to fix that?
+
+			// When in doubt, regex. This is ugly as hell, but it mostly works.
+			// Start by removing all those superflous spaces. Replace HTTP(S):// with slack-workaround:// to mark a URL as handled, so it won't
+			// accidentally be matched again in the next recursion.
 			var newMessage = RecursiveReplace(message.Message, @"^(.*?)(https?:\/\/)(.*?)( )(.*)$", "$1slack-workaround://$3$5");
 			newMessage = newMessage.Replace("slack-workaround://", "http://");
+			// Now let's get rid of those superfluous URLs.
 			newMessage = RecursiveReplace(newMessage, @"^(.*? |)(.+)( https?:\/\/\2)(.*)$", "$1$2$4");
 
 			return new IrcMessage
