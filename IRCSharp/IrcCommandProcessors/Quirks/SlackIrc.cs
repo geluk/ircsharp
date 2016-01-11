286209bcc0bf357497869694dff57936e8972a8d
Johan Geluk <jhgeluk@gmail.com>
2016-01-11 12:01:31 +0100

    Fix a typo

--- IRCSharp/IrcCommandProcessors/Quirks/SlackIrc.cs
+++ IRCSharp/IrcCommandProcessors/Quirks/SlackIrc.cs
@@ -12,7 +12,7 @@ namespace IRCSharp.IrcCommandProcessors.Quirks
 {
 	public class SlackIrcProcessor : DataProcessor
 	{
-		private string RecuriveReplace(string input, string pattern, string replacement)
+		private string RecursiveReplace(string input, string pattern, string replacement)
 		{
 			string currentMessage;
 			string newMessage = input;
@@ -26,9 +26,9 @@ namespace IRCSharp.IrcCommandProcessors.Quirks
 
 		public override IrcMessage ProcessMessage(IrcMessage message)
 		{
-			var newMessage = RecuriveReplace(message.Message, @"^(.*?)(https?:\/\/)(.*?)( )(.*)$", "$1slack-workaround://$3$5");
+			var newMessage = RecursiveReplace(message.Message, @"^(.*?)(https?:\/\/)(.*?)( )(.*)$", "$1slack-workaround://$3$5");
 			newMessage = newMessage.Replace("slack-workaround://", "http://");
-			newMessage = RecuriveReplace(newMessage, @"^(.*? |)(.+)( https?:\/\/\2)(.*)$", "$1$2$4");
+			newMessage = RecursiveReplace(newMessage, @"^(.*? |)(.+)( https?:\/\/\2)(.*)$", "$1$2$4");
 
 			return new IrcMessage
 			{
