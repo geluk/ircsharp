9167155d29d0655749eafa7e632ae5241bfd8081
Johan Geluk <jhgeluk@gmail.com>
2016-01-11 12:20:28 +0100

    Because wording matters

--- IRCSharp/IrcCommandProcessors/Quirks/SlackIrc.cs
+++ IRCSharp/IrcCommandProcessors/Quirks/SlackIrc.cs
@@ -43,7 +43,7 @@ namespace IRCSharp.IrcCommandProcessors.Quirks
 			// accidentally be matched again in the next recursion.
 			var newMessage = RecursiveReplace(message.Message, @"^(.*?)(https?:\/\/)(.*?)( )(.*)$", "$1slack-workaround://$3$5");
 			newMessage = newMessage.Replace("slack-workaround://", "http://");
-			// Now let's get rid of those superfluous URLs.
+			// Now let's get rid of those extra URLs.
 			newMessage = RecursiveReplace(newMessage, @"^(.*? |)(.+)( https?:\/\/\2)(.*)$", "$1$2$4");
 
 			return new IrcMessage
