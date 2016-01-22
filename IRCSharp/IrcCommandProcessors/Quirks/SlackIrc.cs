afc69f5a492ea408ee85a5f66f9c702a8f0bca02
Johan Geluk <jhgeluk@gmail.com>
2016-01-22 12:10:14 +0100

    Removed unused using directives

--- IRCSharp/IrcCommandProcessors/Quirks/SlackIrc.cs
+++ IRCSharp/IrcCommandProcessors/Quirks/SlackIrc.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Xml.Schema;
+﻿using System.Text.RegularExpressions;
 using IRCSharp.IRC;
 
 namespace IRCSharp.IrcCommandProcessors.Quirks
